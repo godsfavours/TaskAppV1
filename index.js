7e1357812a60f77dfa5569c649d9b06845232494
Godsfavour <godsfavours@gmail.com>
2020-12-23 01:13:09 -0600

    Create DB Schema & Model

--- index.js
+++ index.js
@@ -4,15 +4,36 @@ const path = require('path');
 
 // Mongoose connection
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/tasksDB', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log("Error connecting to Database.");
-  })
-  .catch(err => {
-    console.log("An error occured while attempting to connect to DB");
-    console.log(err);
-  });
+mongoose.connect('mongodb://localhost/tasksDB', { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+  console.log("Error connecting to Database.");
+}).catch(err => {
+  console.log("An error occured while attempting to connect to DB");
+  console.log(err);
+});
 
+// DB Schemas & Models
+const taskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: false
+  },
+  lastUpdated: {
+    type: Date,
+    required: true
+  },
+  priority: {
+    type: String,
+    enum: ['Low', 'Medium', 'High']
+  }
+})
+
+const Task = mongoose.model('Task', taskSchema);
+
+// Application routes
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'pages'));
 
